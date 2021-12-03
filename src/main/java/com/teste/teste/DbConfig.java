0e2ac6fbad7647a31bfa99d4553e93764890013f
Carlos Augusto <carlos.fernandes@ifrj.edu.br>
2021-12-03 17:02:43 -0300

    alterei o BD para PostgreSql

--- src/main/java/com/teste/teste/DbConfig.java
+++ src/main/java/com/teste/teste/DbConfig.java
@@ -15,20 +15,26 @@ public class DbConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/crypto");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
+        //dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		//dataSource.setUrl("jdbc:mysql://localhost:3306/crypto");
+		//dataSource.setUsername("root");
+		//dataSource.setPassword("");
+		dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/crypto");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("1234");
         return dataSource;
     }
 	
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
+		//adapter.setDatabase(Database.MYSQL);
+		//adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setDatabase(Database.POSTGRESQL);
+		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
 		adapter.setPrepareConnection(true);
 		return adapter;
 	}
