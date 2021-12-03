ebe46d404e1d46d1010e62c1dc1e7aa501b97d69
Carlos Augusto <carlos.fernandes@ifrj.edu.br>
2021-12-03 19:59:00 -0300

    retirei o lombok

--- src/main/java/com/teste/teste/Compras.java
+++ src/main/java/com/teste/teste/Compras.java
@@ -1,12 +1,12 @@
 package com.teste.teste;
 
 import javax.persistence.*;
-import lombok.*;
+//import lombok.*;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Compras {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +16,54 @@ public class Compras {
     Double quantidade;
     Double preco_total;
 
-    
+    public Compras(){
+        
+    }
+    public Compras(Long id, String descricao, Double preco, Double quantidade, Double preco_total) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.preco_total = preco_total;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco_total() {
+        return preco_total;
+    }
+
+    public void setPreco_total(Double preco_total) {
+        this.preco_total = preco_total;
+    }
 }
